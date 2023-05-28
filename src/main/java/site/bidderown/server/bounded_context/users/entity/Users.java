3b0a9f7b3193eb0e942a550a6620a781eee7b722
Eui9179 <outdark97@gmail.com>
2023-05-29 02:06:18 +0900

    feature/users-기본-entity-추가

---
+++ src/main/java/site/bidderown/server/bounded_context/users/entity/Users.java
@@ -0,0 +1,20 @@
+package site.bidderown.server.bounded_context.users.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import lombok.experimental.SuperBuilder;
+import site.bidderown.server.base.base_entity.BaseEntity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+
+@Getter
+@NoArgsConstructor
+@ToString(callSuper = true)
+@SuperBuilder
+@Entity
+public class Users extends BaseEntity {
+    @Column(unique = true)
+    private String name;
+}
