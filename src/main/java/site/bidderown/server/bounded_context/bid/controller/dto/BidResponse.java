7d1a5314604f0a30f264b51ad9557b7a8ddd6420
gkfktkrh153 <gkfktkrh153@naver.com>
2023-06-01 18:12:01 +0900

    Feature : 경매 도메인에 대한 기초 작업을 진행합니다.

---
+++ src/main/java/site/bidderown/server/bounded_context/bid/controller/dto/BidResponse.java
@@ -0,0 +1,36 @@
+package site.bidderown.server.bounded_context.bid.controller.dto;
+
+import lombok.*;
+import site.bidderown.server.bounded_context.bid.entity.Bid;
+import site.bidderown.server.bounded_context.item.entity.Item;
+
+@Setter
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class BidResponse {
+    private String bidderName;
+    private int price; // 최소 희망 금액
+    private int bidPrice; // 입찰 금액
+    private Long itemId;
+    // img도 들어가야할 것 같습니다.
+    //private Long maxPrice;
+    //private Long minPrice;
+    //private Long avgPrice;
+
+
+    @Builder
+    private BidResponse(String bidderName, int price, int bidPrice, Long itemId) {
+        this.bidderName = bidderName;
+        this.price = price;
+        this.bidPrice = bidPrice;
+        this.itemId = itemId;
+    }
+
+    public static BidResponse of(Bid bid, Item item){
+        return BidResponse.builder()
+                .bidderName(bid.getBidder().getName())
+                .price(item.getMinimumPrice())
+                .bidPrice(bid.getPrice())
+                .itemId(item.getId()).build();
+    }
+}
---
+++ src/main/java/site/bidderown/server/bounded_context/bid/entity/Bid.java
@@ -0,0 +1,46 @@
+package site.bidderown.server.bounded_context.bid.entity;
+
+import lombok.*;
+import site.bidderown.server.base.base_entity.BaseEntity;
+import site.bidderown.server.bounded_context.bid.controller.dto.BidRequest;
+import site.bidderown.server.bounded_context.item.entity.Item;
+import site.bidderown.server.bounded_context.member.entity.Member;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class Bid extends BaseEntity
+{
+
+    private int price;
+
+    @Enumerated(value = EnumType.STRING)
+    private BidResult bidResult;
+
+    @ManyToOne
+    @JoinColumn(nullable = false)
+    private Member bidder;
+
+    @ManyToOne
+    @JoinColumn
+    private Item item;
+
+    @Builder
+    private Bid(int price, BidResult bidResult, Member bidder, Item item) {
+        this.price = price;
+        this.bidResult = bidResult;
+        this.bidder = bidder;
+        this.item = item;
+    }
+
+    public static Bid of(BidRequest bidRequest, Member bidder, Item item){
+        return Bid.builder()
+                .price(bidRequest.getItemPrice())
+                .bidResult(BidResult.WAIT)
+                .bidder(bidder)
+                .item(item)
+                .build();
+    }
+}
