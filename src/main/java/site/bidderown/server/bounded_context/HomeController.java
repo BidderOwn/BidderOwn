9aebb4cf0d0bbe4ec52467c5b271a227c2ba150c
Eui9179 <outdark97@gmail.com>
2023-06-11 01:11:46 +0900

    home-ui

--- src/main/java/site/bidderown/server/bounded_context/HomeController.java
+++ src/main/java/site/bidderown/server/bounded_context/HomeController.java
@@ -1,15 +1,21 @@
 package site.bidderown.server.bounded_context;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import site.bidderown.server.bounded_context.member.entity.Member;
+import site.bidderown.server.bounded_context.member.service.MemberService;
 
 @Slf4j
+@RequiredArgsConstructor
 @Controller
 public class HomeController {
+    private final MemberService memberService;
 
     @GetMapping("/login")
     public String testLogin() {
@@ -17,8 +23,9 @@ public class HomeController {
     }
 
     @GetMapping("/")
-    public String home(Model model, @AuthenticationPrincipal User user) {
-        model.addAttribute("username", user.getUsername());
+    public String home(Model model){//, @AuthenticationPrincipal User user) {
+        Member user = memberService.getMember("user_1");
+        model.addAttribute("username", user.getName());
         return "/usr/item/home";
     }
 }
