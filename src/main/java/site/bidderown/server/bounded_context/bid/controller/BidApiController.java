5c50974768468b900dfe6cc4ece94773fb8491f2
JinseoPark-bd <wlstjdhkd@naver.com>
2023-06-21 17:31:24 +0900

    Feature:151-bid-delete

--- src/main/java/site/bidderown/server/bounded_context/bid/controller/BidApiController.java
+++ src/main/java/site/bidderown/server/bounded_context/bid/controller/BidApiController.java
@@ -1,14 +1,20 @@
 package site.bidderown.server.bounded_context.bid.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 import site.bidderown.server.bounded_context.bid.controller.dto.BidDetails;
 import site.bidderown.server.bounded_context.bid.controller.dto.BidRequest;
 import site.bidderown.server.bounded_context.bid.controller.dto.BidResponse;
 import site.bidderown.server.bounded_context.bid.controller.dto.BidResponses;
 import site.bidderown.server.bounded_context.bid.service.BidService;
+import site.bidderown.server.bounded_context.item.service.ItemService;
+import site.bidderown.server.bounded_context.member.controller.dto.MemberDetail;
+import site.bidderown.server.bounded_context.member.service.MemberService;
 
 import java.util.List;
 
@@ -17,6 +23,7 @@ import java.util.List;
 public class BidApiController {
 
     private final BidService bidService;
+    private final MemberService memberService;
 
     @PostMapping("/api/v1/bid")
     public Long registerBid(@RequestBody BidRequest bidRequest, @AuthenticationPrincipal User user){
@@ -36,7 +43,18 @@ public class BidApiController {
     ) {
         return bidService.getBidItemIds(user.getUsername());
     }
+    @DeleteMapping("/api/v1/bid/{bidId}")
+    @PreAuthorize("isAuthenticated()")
+    @ResponseBody
+    public String deleteBid(@PathVariable Long bidId, @AuthenticationPrincipal User user) {
+        MemberDetail memberDetail = MemberDetail.of(memberService.getMember(user.getUsername()));
 
+        if (!memberDetail.getName().equals(user.getUsername())) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "삭제권한이 없습니다.");
+        }
 
+        bidService.delete(bidId);
+        return "ok";
+    }
 }
 
