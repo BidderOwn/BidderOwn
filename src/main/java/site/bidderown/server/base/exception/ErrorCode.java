74731b8e115afda40e011a4989d04603c9ce426f
gkfktkrh153 <gkfktkrh153@naver.com>
2023-06-22 19:00:12 +0900

    commit1

---
+++ src/main/java/site/bidderown/server/base/exception/ErrorCode.java
@@ -0,0 +1,20 @@
+package site.bidderown.server.base.exception;
+
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+public enum ErrorCode {
+    NOT_FOUND(HttpStatus.NOT_FOUND, "조회하려고 하는 데이터가 존재하지 않을 경우"),
+    FORBIDDEN(HttpStatus.FORBIDDEN, "권한이 없는 경우"),
+    DUFLICATION(HttpStatus.BAD_REQUEST, "데이터 중복"),
+    BID_END(HttpStatus.BAD_REQUEST, "입찰이 종료된 경우");
+
+    private final HttpStatus status;
+    private final String description;
+
+    ErrorCode(HttpStatus status, String description) {
+        this.status = status;
+        this.description = description;
+    }
+}
---
+++ src/main/java/site/bidderown/server/base/exception/ErrorResponse.java
@@ -0,0 +1,5 @@
+package site.bidderown.server.base.exception;
+
+public record ErrorResponse(
+        String message
+){}
