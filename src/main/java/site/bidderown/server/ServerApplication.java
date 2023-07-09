cf0a2ce28bc893926bc82d1bb4e1cf574afc7bf0
seungnYong <77851079+gkfktkrh153@users.noreply.github.com>
2023-07-10 04:35:34 +0900

    Update ServerApplication.java

--- src/main/java/site/bidderown/server/ServerApplication.java
+++ src/main/java/site/bidderown/server/ServerApplication.java
@@ -17,7 +17,7 @@ import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 public class ServerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ServerApplication.class, args);
+		SpringApplication.run(ServerApplication.class, args);//
 	}
 
 	@Bean
