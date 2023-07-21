cdd33b9c1671c79425ec1e79f6a484a591ca6dd9
Eui9179 <outdark97@gmail.com>
2023-07-22 02:48:22 +0900

    main-leui

--- src/main/java/site/bidderown/server/base/redis/config/RedisConfig.java
+++ src/main/java/site/bidderown/server/base/redis/config/RedisConfig.java
@@ -1,10 +1,5 @@
 package site.bidderown.server.base.redis.config;
 
-import com.fasterxml.jackson.annotation.JsonTypeInfo;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.jsontype.BasicPolymorphicTypeValidator;
-import com.fasterxml.jackson.databind.jsontype.PolymorphicTypeValidator;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
@@ -18,15 +13,12 @@ import org.springframework.data.redis.listener.PatternTopic;
 import org.springframework.data.redis.listener.RedisMessageListenerContainer;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
-import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
-import site.bidderown.server.base.redis.buffer.BufferTask;
 import site.bidderown.server.base.redis.listener.ExpirationListener;
 
 @RequiredArgsConstructor
 @Slf4j
 @Configuration
-@EnableRedisRepositories(enableKeyspaceEvents = RedisKeyValueAdapter.EnableKeyspaceEvents.ON_STARTUP)
 public class RedisConfig {
 
     @Value("${spring.redis.host}")
