55c12ce0a88bdca0d8b8278379a2ba9a20755635
Eui9179 <outdark97@gmail.com>
2023-08-07 21:55:27 +0900

    chore: 문구-변경

--- src/main/java/site/bidderown/server/base/exception/custom_exception/LowerBidPriceException.java
+++ src/main/java/site/bidderown/server/base/exception/custom_exception/LowerBidPriceException.java
@@ -7,6 +7,6 @@ public class LowerBidPriceException extends CustomException {
     private static final ErrorCode errorCode = ErrorCode.LOWER_BID_PRICE;
 
     public LowerBidPriceException(Long id){
-        super(errorCode, "더 낮은 값을 제시할 수 없습니다.", "Bid with a lower price. item id: " + id);
+        super(errorCode, "잘못된 입찰가 제시", "Bid with a wrong price. item id: " + id);
     }
 }
