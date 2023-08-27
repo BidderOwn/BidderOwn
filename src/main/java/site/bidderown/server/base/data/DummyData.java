db7c18db63e5f815f0e19d713cb14837e443e47c
Eui9179 <outdark97@gmail.com>
2023-08-27 23:14:56 +0900

    chore: 더미데이터-삭제

--- src/main/java/site/bidderown/server/base/data/DummyData.java
+++ src/main/java/site/bidderown/server/base/data/DummyData.java
@@ -23,19 +23,7 @@ public class DummyData {
     }
 
     private void initMembers() {
-        Member member1 = memberService.join("user1", "1234");
+        memberService.join("user1", "1234");
         memberService.join("user2", "1234");
-
-        for (int i = 0; i < 30; i++) {
-            itemRepository.save(
-                    Item.builder()
-                            .title("test")
-                            .description("desc")
-                            .expireAt(LocalDateTime.of(2023, 12, 31, 0, 0))
-                            .member(member1)
-                            .minimumPrice(1000)
-                            .build()
-            );
-        }
     }
 }
