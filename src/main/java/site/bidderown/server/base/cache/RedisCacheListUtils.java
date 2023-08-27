e24f0098c7affd021927ca245154d86010d08da6
Eui9179 <outdark97@gmail.com>
2023-08-27 23:07:08 +0900

    refactoring: update-code

--- src/main/java/site/bidderown/server/base/cache/RedisCacheListUtils.java
+++ src/main/java/site/bidderown/server/base/cache/RedisCacheListUtils.java
@@ -12,7 +12,8 @@ public class RedisCacheListUtils {
 
     public void removeKeyPattern(String keyPattern) {
         redisTemplate.execute((RedisCallback<Object>) connection -> {
-            Cursor<byte[]> cursor = connection.scan(ScanOptions.scanOptions().match(keyPattern + "*").build());
+            ScanOptions pattern = ScanOptions.scanOptions().match(keyPattern + "*").build();
+            Cursor<byte[]> cursor = connection.scan(pattern);
             while (cursor.hasNext()) {
                 byte[] key = cursor.next();
                 redisTemplate.delete(new String(key));
